e33233c076f9ac904aeac556ed9e8cc9e724bf47
Sertunc <sertuncselen@yahoo.com>
2026-01-31 22:17:28 +0300

    Adjust phone coordinates in config.ts

--- src/templates/Template3/config.ts
+++ src/templates/Template3/config.ts
@@ -13,7 +13,7 @@ export const config: TemplateConfig = {
     regardlessBloodGroup: { coord: { top: 75, left: 70 }, font: { size: 37, color: "#89e8e0" } },
     bloodType: { coord: { top: 171, left: 150 }, font: { size: 17, color: "#000000" } },
     fullName: { coord: { top: 348, left: 102 }, font: { size: 17, color: "#000000" } },
-    phone: { coord: { top: 302, left: 65 }, font: { size: 17, color: "#000000" } },
+    phone: { coord: { top: 270, left: 86 }, font: { size: 17, color: "#000000" } },
     date: { coord: { top: 222, left: 70 }, font: { size: 17, color: "#000000" } },
     hospital: { coord: { top: 418, left: 31 }, font: { size: 16, color: "#000000" } },
     location: { coord: { top: 470, left: 31 }, font: { size: 16, color: "#000000" } },
